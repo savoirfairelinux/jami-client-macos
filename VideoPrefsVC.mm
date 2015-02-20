d2e02ad6a1a7ec4cea083c78d4cd67851954cbfb
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2015-02-20 10:06:43 -0500

    ui: add Preference screen
    - enter/exit animation
    - switch between panels (general/audio/video/hooks)
    
    Refs #66840
    
    Change-Id: I10b823c5e13c6667f1303cd51e32531a375828ff

---
+++ VideoPrefsVC.mm
@@ -0,0 +1,22 @@
+//
+//  VideoPrefsVC.m
+//  Ring
+//
+//  Created by Alexandre Lision on 2015-02-20.
+//
+//
+
+#import "VideoPrefsVC.h"
+
+@interface VideoPrefsVC ()
+
+@end
+
+@implementation VideoPrefsVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
