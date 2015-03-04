28629c75ccff144cca75048d19a9b91dddea3cc6
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2015-03-04 15:10:35 -0500

    work on call screen
    - in/out animation
    - switch calls in conversation tree
    - basic controls over call: accept/hangup/hold
    
    Refs #67611
    
    Change-Id: I1d302d2539cb473e27ea469781b72f44963eb0a0

--- HistoryViewController.h
+++ HistoryViewController.h
@@ -31,15 +31,10 @@
 #define HISTORYVIEWCONTROLLER_H
 
 #import <Cocoa/Cocoa.h>
-#import "QNSTreeController.h"
 
 @interface HistoryViewController : NSViewController <NSOutlineViewDelegate> {
 
-    NSOutlineView *historyView;
 }
-
-@property NSTreeController *treeController;
-@property (assign) IBOutlet NSOutlineView *historyView;
 
 @end
 
