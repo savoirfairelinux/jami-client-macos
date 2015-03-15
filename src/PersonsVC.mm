c751aeef27e49a7344b349c7977f5f30a561397d
Alexandre Lision <alexandre.lision@gmail.com>
2015-03-15 18:43:07 -0400

    contacts: add backend and new view controller
    Change-Id: I8d897802b086eccc42446d58f3efa83cb953e14b

---
+++ src/PersonsVC.mm
@@ -0,0 +1,157 @@
+/*
+ *  Copyright (C) 2004-2015 Savoir-Faire Linux Inc.
+ *  Author: Alexandre Lision <alexandre.lision@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  If you modify this program, or any covered work, by linking or
+ *  combining it with the OpenSSL project's OpenSSL library (or a
+ *  modified version of that library), containing parts covered by the
+ *  terms of the OpenSSL or SSLeay licenses, Savoir-Faire Linux Inc.
+ *  grants you additional permission to convey the resulting work.
+ *  Corresponding Source for a non-source form of such a combination
+ *  shall include the source code for the parts of OpenSSL used as well
+ *  as that of the covered work.
+ */
+
+#import "PersonsVC.h"
+
+#import <personmodel.h>
+#import <person.h>
+#import <contactmethod.h>
+
+#import "backends/AddressBookBackend.h"
+#import "QNSTreeController.h"
+
+#define COLUMNID_NAME @"NameColumn"
+#define COLUMNID_PHOTO @"ImageColumn"
+
+
+@interface PersonsVC ()
+
+@property NSTreeController *treeController;
+@property (assign) IBOutlet NSOutlineView *personsView;
+
+@end
+
+@implementation PersonsVC
+@synthesize treeController;
+@synthesize personsView;
+
+-(void) awakeFromNib
+{
+    NSLog(@"INIT PersonsVC");
+    treeController = [[QNSTreeController alloc] initWithQModel:PersonModel::instance()];
+
+    [treeController setAvoidsEmptySelection:NO];
+    [treeController setChildrenKeyPath:@"children"];
+
+    [personsView bind:@"content" toObject:treeController withKeyPath:@"arrangedObjects" options:nil];
+    [personsView bind:@"sortDescriptors" toObject:treeController withKeyPath:@"sortDescriptors" options:nil];
+    [personsView bind:@"selectionIndexPaths" toObject:treeController withKeyPath:@"selectionIndexPaths" options:nil];
+
+    PersonModel::instance()->addCollection<AddressBookBackend>(LoadOptions::FORCE_ENABLED);
+
+}
+
+#pragma mark - NSOutlineViewDelegate methods
+
+// -------------------------------------------------------------------------------
+//	shouldSelectItem:item
+// -------------------------------------------------------------------------------
+- (BOOL)outlineView:(NSOutlineView *)outlineView shouldSelectItem:(id)item;
+{
+    return YES;
+}
+
+// -------------------------------------------------------------------------------
+//	dataCellForTableColumn:tableColumn:item
+// -------------------------------------------------------------------------------
+- (NSCell *)outlineView:(NSOutlineView *)outlineView dataCellForTableColumn:(NSTableColumn *)tableColumn item:(id)item
+{
+    NSCell *returnCell = [tableColumn dataCell];
+    if(item == nil)
+        return returnCell;
+    return returnCell;
+}
+
+// -------------------------------------------------------------------------------
+//	textShouldEndEditing:fieldEditor
+// -------------------------------------------------------------------------------
+- (BOOL)control:(NSControl *)control textShouldEndEditing:(NSText *)fieldEditor
+{
+    if ([[fieldEditor string] length] == 0)
+    {
+        // don't allow empty node names
+        return NO;
+    }
+    else
+    {
+        return YES;
+    }
+}
+
+// -------------------------------------------------------------------------------
+//	shouldEditTableColumn:tableColumn:item
+//
+//	Decide to allow the edit of the given outline view "item".
+// -------------------------------------------------------------------------------
+- (BOOL)outlineView:(NSOutlineView *)outlineView shouldEditTableColumn:(NSTableColumn *)tableColumn item:(id)item
+{
+    return YES;
+}
+
+// -------------------------------------------------------------------------------
+//	outlineView:willDisplayCell:forTableColumn:item
+// -------------------------------------------------------------------------------
+- (void)outlineView:(NSOutlineView *)olv willDisplayCell:(NSCell*)cell forTableColumn:(NSTableColumn *)tableColumn item:(id)item
+{
+    QModelIndex qIdx = [treeController toQIdx:((NSTreeNode*)item)];
+    if(!qIdx.isValid())
+        return;
+
+    if ([[tableColumn identifier] isEqualToString:COLUMNID_NAME])
+    {
+        if(!qIdx.parent().isValid()) {
+            cell.title = PersonModel::instance()->data(qIdx, Qt::DisplayRole).toString().toNSString();
+        } else {
+            ContactMethod* meth = PersonModel::instance()->data(qIdx, (int)ContactMethod::Role::Object).value<ContactMethod*>();
+        }
+
+
+    } else if ([[tableColumn identifier] isEqualToString:COLUMNID_PHOTO]) {
+        if(qIdx.isValid()) {
+            Person* p = PersonModel::instance()->data(qIdx, (int)Person::Role::Object).value<Person*>();
+            //NSLog(@"====== Photo: %@", p->photo().value<QByteArray>().toNSData());
+        }
+    }
+}
+
+// -------------------------------------------------------------------------------
+//	outlineViewSelectionDidChange:notification
+// -------------------------------------------------------------------------------
+- (void)outlineViewSelectionDidChange:(NSNotification *)notification
+{
+    // ask the tree controller for the current selection
+    if([[treeController selectedNodes] count] > 0) {
+        QModelIndex qIdx = [treeController toQIdx:[treeController selectedNodes][0]];
+        //Update details view by changing selection
+    }
+}
+
+
+@end
---
+++ src/backends/AddressBookBackend.mm
@@ -0,0 +1,243 @@
+/*
+ *  Copyright (C) 2004-2015 Savoir-Faire Linux Inc.
+ *  Author: Alexandre Lision <alexandre.lision@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  If you modify this program, or any covered work, by linking or
+ *  combining it with the OpenSSL project's OpenSSL library (or a
+ *  modified version of that library), containing parts covered by the
+ *  terms of the OpenSSL or SSLeay licenses, Savoir-Faire Linux Inc.
+ *  grants you additional permission to convey the resulting work.
+ *  Corresponding Source for a non-source form of such a combination
+ *  shall include the source code for the parts of OpenSSL used as well
+ *  as that of the covered work.
+ */
+#include "AddressBookBackend.h"
+
+#import <AddressBook/AddressBook.h>
+
+//Qt
+#include <QtCore/QFile>
+#include <QtCore/QDir>
+#include <QtCore/QHash>
+#include <QtWidgets/QApplication>
+#include <QtCore/QStandardPaths>
+
+//Ring
+#include <Person.h>
+#include <account.h>
+#include <person.h>
+#include <contactmethod.h>
+
+/**
+ *
+ *kABFirstNameProperty
+ kABLastNameProperty
+ kABFirstNamePhoneticProperty
+ kABLastNamePhoneticProperty
+ kABBirthdayProperty
+ kABOrganizationProperty
+ kABJobTitleProperty
+ kABHomePageProperty
+ kABEmailProperty
+ kABAddressProperty
+ kABPhoneProperty
+ kABAIMInstantProperty
+ kABJabberInstantProperty
+ kABMSNInstantProperty
+ kABYahooInstantProperty
+ kABICQInstantProperty
+ kABNoteProperty
+ kABMiddleNameProperty
+ kABMiddleNamePhoneticProperty
+ kABTitleProperty
+ kABSuffixProperty
+ kABNicknameProperty
+ kABMaidenNameProperty
+ */
+
+class AddressBookEditor : public CollectionEditor<Person>
+{
+public:
+    AddressBookEditor(CollectionMediator<Person>* m, AddressBookBackend* parent);
+    virtual bool save       ( const Person* item ) override;
+    virtual bool remove     ( const Person* item ) override;
+    virtual bool edit       ( Person*       item ) override;
+    virtual bool addNew     ( const Person* item ) override;
+    virtual bool addExisting( const Person* item ) override;
+
+private:
+    virtual QVector<Person*> items() const override;
+
+    //Helpers
+    void savePerson(QTextStream& stream, const Person* Person);
+    bool regenFile(const Person* toIgnore);
+
+    //Attributes
+    QVector<Person*> m_lItems;
+    AddressBookBackend* m_pCollection;
+};
+
+AddressBookEditor::AddressBookEditor(CollectionMediator<Person>* m, AddressBookBackend* parent) :
+CollectionEditor<Person>(m),m_pCollection(parent)
+{
+
+}
+
+AddressBookBackend::AddressBookBackend(CollectionMediator<Person>* mediator) :
+CollectionInterface(new AddressBookEditor(mediator,this)),m_pMediator(mediator)
+{
+
+}
+
+AddressBookBackend::~AddressBookBackend()
+{
+
+}
+
+void AddressBookEditor::savePerson(QTextStream& stream, const Person* Person)
+{
+
+    qDebug() << "Saving Person!";
+}
+
+bool AddressBookEditor::regenFile(const Person* toIgnore)
+{
+    QDir dir(QString('/'));
+    dir.mkpath(QStandardPaths::writableLocation(QStandardPaths::DataLocation) + QLatin1Char('/') + QString());
+
+
+    return false;
+}
+
+bool AddressBookEditor::save(const Person* Person)
+{
+    //if (Person->collection()->editor<Person>() != this)
+    //    return addNew(Person);
+
+    return regenFile(nullptr);
+}
+
+bool AddressBookEditor::remove(const Person* item)
+{
+    return regenFile(item);
+}
+
+bool AddressBookEditor::edit( Person* item)
+{
+    Q_UNUSED(item)
+    return false;
+}
+
+bool AddressBookEditor::addNew(const Person* Person)
+{
+    QDir dir(QString('/'));
+    dir.mkpath(QStandardPaths::writableLocation(QStandardPaths::DataLocation) + QLatin1Char('/') + QString());
+
+    return false;
+}
+
+bool AddressBookEditor::addExisting(const Person* item)
+{
+    m_lItems << const_cast<Person*>(item);
+    mediator()->addItem(item);
+    return true;
+}
+
+QVector<Person*> AddressBookEditor::items() const
+{
+    return m_lItems;
+}
+
+QString AddressBookBackend::name () const
+{
+    return QObject::tr("AddressBook backend");
+}
+
+QString AddressBookBackend::category () const
+{
+    return QObject::tr("Persons");
+}
+
+QVariant AddressBookBackend::icon() const
+{
+    return QVariant();
+}
+
+bool AddressBookBackend::isEnabled() const
+{
+    return true;
+}
+
+bool AddressBookBackend::load()
+{
+    ABAddressBook *book = [ABAddressBook sharedAddressBook];
+
+    NSArray *everyone = [book people];
+    //NSArray *everygroup = [book groups];
+
+    //NSLog( [everyone description] );
+    //NSLog( [everygroup description] );
+    for(ABPerson* p in everyone)
+    {
+        Person* person = new Person(QByteArray::fromNSData(p.vCardRepresentation),
+                                    Person::Encoding::vCard,
+                                    this);
+        if([person->formattedName().toNSString() isEqualToString:@""]   &&
+            [person->secondName().toNSString() isEqualToString:@""]     &&
+           [person->firstName().toNSString() isEqualToString:@""]) {
+            qDebug() << QByteArray::fromNSData(p.vCardRepresentation);
+            continue;
+        }
+
+
+        qDebug() << "Adding: " << person->formattedName() << " " << person->secondName() << " " << person->firstName();
+
+        person->setCollection(this);
+
+        editor<Person>()->addExisting(person);
+    }
+     return false;
+}
+
+
+bool AddressBookBackend::reload()
+{
+    return false;
+}
+
+CollectionInterface::SupportedFeatures AddressBookBackend::supportedFeatures() const
+{
+    return (CollectionInterface::SupportedFeatures) (
+                                                     CollectionInterface::SupportedFeatures::NONE  |
+                                                     CollectionInterface::SupportedFeatures::LOAD  |
+                                                     CollectionInterface::SupportedFeatures::CLEAR |
+                                                     CollectionInterface::SupportedFeatures::REMOVE|
+                                                     CollectionInterface::SupportedFeatures::ADD   );
+}
+
+bool AddressBookBackend::clear()
+{
+    /* TODO: insert confirm dialog? */
+    return true;
+}
+
+QByteArray AddressBookBackend::id() const
+{
+    return "abb";
+}
