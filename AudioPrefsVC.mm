1996413373b8cafa48c4b1ceb7c33c9b798f78e6
Alexandre Lision <alexandre.lision@gmail.com>
2015-03-20 18:07:17 -0400

    preferences: bind audio preferences to lrc
    Refs #66840
    
    Change-Id: I9176b20cc39642953e3a32a25e35a0b73d1ce8e2

--- AudioPrefsVC.mm
+++ AudioPrefsVC.mm
@@ -29,11 +29,165 @@
  */
 #import "AudioPrefsVC.h"
 
+#import <audio/settings.h>
+#import <QUrl>
+#import <audio/inputdevicemodel.h>
+#import <audio/outputdevicemodel.h>
+#import <qitemselectionmodel.h>
+
+#import "QNSTreeController.h"
+
 @interface AudioPrefsVC ()
+@property (assign) IBOutlet NSPathControl *recordingsPathControl;
 
 @end
 
 @implementation AudioPrefsVC
+@synthesize recordingsPathControl;
+@synthesize outputDeviceList;
+@synthesize inputDeviceList;
+@synthesize alwaysRecordingButton;
+@synthesize muteDTMFButton;
 
+- (void)loadView
+{
+    [super loadView];
+
+    QModelIndex qInputIdx = Audio::Settings::instance()->inputDeviceModel()->selectionModel()->currentIndex();
+    QModelIndex qOutputIdx = Audio::Settings::instance()->outputDeviceModel()->selectionModel()->currentIndex();
+    [self.outputDeviceList addItemWithTitle:Audio::Settings::instance()->outputDeviceModel()->data(qOutputIdx, Qt::DisplayRole).toString().toNSString()];
+    [self.inputDeviceList addItemWithTitle:Audio::Settings::instance()->inputDeviceModel()->data(qInputIdx, Qt::DisplayRole).toString().toNSString()];
+    [self.alwaysRecordingButton setState:
+            Audio::Settings::instance()->isAlwaysRecording()?NSOnState:NSOffState];
+
+    [self.muteDTMFButton setState:
+            Audio::Settings::instance()->areDTMFMuted()?NSOnState:NSOffState];
+
+}
+
+- (IBAction)toggleMuteDTMF:(NSButton *)sender
+{
+    Audio::Settings::instance()->setDTMFMuted([sender state] == NSOnState);
+}
+
+- (IBAction)toggleAlwaysRecording:(NSButton *)sender
+{
+    Audio::Settings::instance()->setAlwaysRecording([sender state] == NSOnState);
+}
+
+- (IBAction)pathControlSingleClick:(id)sender {
+    // Select that chosen component of the path.
+    [recordingsPathControl setURL:[[recordingsPathControl clickedPathComponentCell] URL]];
+
+    NSLog(@"setting Recording path to: %@", [[[recordingsPathControl clickedPathComponentCell] URL] absoluteString]);
+
+    Audio::Settings::instance()->setRecordPath(QUrl::fromNSURL([[recordingsPathControl clickedPathComponentCell] URL]));
+
+}
+
+#pragma mark - NSPathControl delegate methods
+
+/*
+ Delegate method of NSPathControl to determine how the NSOpenPanel will look/behave.
+ */
+- (void)pathControl:(NSPathControl *)pathControl willDisplayOpenPanel:(NSOpenPanel *)openPanel
+{
+    NSLog(@"willDisplayOpenPanel");
+    [openPanel setAllowsMultipleSelection:NO];
+    [openPanel setCanChooseDirectories:YES];
+    [openPanel setCanChooseFiles:NO];
+    [openPanel setResolvesAliases:YES];
+    [openPanel setTitle:NSLocalizedString(@"Choose a file", @"Open panel title")];
+    [openPanel setPrompt:NSLocalizedString(@"Choose", @"Open panel prompt for 'Choose a directory'")];
+    [openPanel setDelegate:self];
+}
+
+- (void)pathControl:(NSPathControl *)pathControl willPopUpMenu:(NSMenu *)menu
+{
+
+}
+
+#pragma mark - NSOpenSavePanelDelegate delegate methods
+
+- (void)panel:(id)sender willExpand:(BOOL)expanding
+{
+    NSLog(@"willExpand");
+}
+
+- (NSString *)panel:(id)sender userEnteredFilename:(NSString *)filename confirmed:(BOOL)okFlag
+{
+    NSLog(@"userEnteredFilename");
+}
+
+- (void)panelSelectionDidChange:(id)sender
+{
+    NSLog(@"panelSelectionDidChange");
+}
+
+- (BOOL)panel:(id)sender validateURL:(NSURL *)url error:(NSError **)outError
+{
+    NSLog(@"validateURL %@", [[[recordingsPathControl clickedPathComponentCell] URL] absoluteString]);
+    return YES;
+
+}
+
+- (void)panel:(id)sender didChangeToDirectoryURL:(NSURL *)url
+{
+    //NSLog(@"didChangeToDirectoryURL");
+}
+
+
+
+#pragma mark - NSMenuDelegate methods
+
+- (BOOL)menuHasKeyEquivalent:(NSMenu *)menu
+                    forEvent:(NSEvent *)event
+                      target:(id *)target
+                      action:(SEL *)action
+{
+    NSLog(@"menuHasKeyEquivalent");
+    return YES;
+}
+
+- (BOOL)menu:(NSMenu *)menu updateItem:(NSMenuItem *)item atIndex:(NSInteger)index shouldCancel:(BOOL)shouldCancel
+{
+    NSLog(@"updateItem");
+    QModelIndex qIdx;
+
+    if([menu.title isEqualToString:@"inputlist"])
+    {
+        qIdx = Audio::Settings::instance()->inputDeviceModel()->index(index);
+        [item setTitle:Audio::Settings::instance()->inputDeviceModel()->data(qIdx, Qt::DisplayRole).toString().toNSString()];
+    } else
+    {
+        qIdx = Audio::Settings::instance()->outputDeviceModel()->index(index);
+        [item setTitle:Audio::Settings::instance()->outputDeviceModel()->data(qIdx, Qt::DisplayRole).toString().toNSString()];
+    }
+
+    return YES;
+}
+
+- (void)menu:(NSMenu *)menu willHighlightItem:(NSMenuItem *)item
+{
+    NSLog(@"willHighlightItem");
+}
+
+- (void)menuWillOpen:(NSMenu *)menu
+{
+    NSLog(@"menuWillOpen");
+}
+
+- (void)menuDidClose:(NSMenu *)menu
+{
+    NSLog(@"menuDidClose");
+}
+
+- (NSInteger)numberOfItemsInMenu:(NSMenu *)menu
+{
+    if([menu.title isEqualToString:@"inputlist"])
+        return Audio::Settings::instance()->inputDeviceModel()->rowCount();
+    else
+        return Audio::Settings::instance()->outputDeviceModel()->rowCount();
+}
 
 @end
