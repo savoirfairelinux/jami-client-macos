d0812a21482f8c6e5177492ad8c9d2783f90d6d2
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2015-09-09 15:56:30 -0400

    ui: override default theme
    This commit overide style for tree items rendering, to fit with wireframes.
    It adds a new Highlight shadow, new item separator, new Selected style.
    Items can also have an array of "contextual views" that will be rendered only
    on mouse hovers events.
    It also introduce support for Ring official colors.
    
    Issue: #79400, #79398
    Change-Id: I8d227955f9afe25240b1ece991c57a3cbf996474

--- src/views/RingOutlineView.mm
+++ src/views/RingOutlineView.mm
@@ -19,6 +19,8 @@
 
 #import "RingOutlineView.h"
 
+#import "HoverTableRowView.h" // For the grid drawing shared code
+
 @implementation RingOutlineView
 
 - (NSMenu*)menuForEvent:(NSEvent*)evt
@@ -52,6 +54,48 @@
         }
     } else
         [super keyDown:theEvent];
+}
+
+- (CGFloat)yPositionPastLastRow {
+    // Only draw the grid past the last visible row
+    NSInteger numberOfRows = self.numberOfRows;
+    CGFloat yStart = 0;
+    if (numberOfRows > 0) {
+        yStart = NSMaxY([self rectOfRow:numberOfRows - 1]);
+    }
+    return yStart;
+}
+
+- (void)drawGridInClipRect:(NSRect)clipRect {
+    // Only draw the grid past the last visible row
+    CGFloat yStart = [self yPositionPastLastRow];
+    // Draw the first separator one row past the last row
+    yStart += self.rowHeight;
+
+    // One thing to do is smarter clip testing to see if we actually need to draw!
+    NSRect boundsToDraw = self.bounds;
+    NSRect separatorRect = boundsToDraw;
+    separatorRect.size.height = 1;
+    while (yStart < NSMaxY(boundsToDraw)) {
+        separatorRect.origin.y = yStart;
+        DrawSeparatorInRect(separatorRect);
+        yStart += self.rowHeight;
+    }
+}
+
+- (void)setFrameSize:(NSSize)size {
+    [super setFrameSize:size];
+    // We need to invalidate more things when live-resizing since we fill with a gradient and stroke
+    if ([self inLiveResize]) {
+        CGFloat yStart = [self yPositionPastLastRow];
+        if (NSHeight(self.bounds) > yStart) {
+            // Redraw our horizontal grid lines
+            NSRect boundsPastY = self.bounds;
+            boundsPastY.size.height -= yStart;
+            boundsPastY.origin.y = yStart;
+            [self setNeedsDisplayInRect:boundsPastY];
+        }
+    }
 }
 
 @end
