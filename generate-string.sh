b132128b090efd21476ddee42fc2006e889e9efe
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2015-09-15 10:25:17 -0400

    i18n: integrate translations to packaging
    This commit adds internationalization support
    We interface with Transifex as our web translation platform.
    
    Two new scripts:
    - generate-strings.sh: generates source files in English. we need one source
    file by UI file (Apple's law) + one Localizable.strings for sintrgs in code.
    
    - update-translations.sh: pulls translations from transifex, and cleanup the
    files (UTF-16LE to UTF-8 encoding, and remove first line with corrupted char)
    
    Issue: #80347
    Change-Id: Iefd297e424aaacefe6e867192e10dabd21bfb8d2

---
+++ generate-string.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This scripts generates .strings files for Base language (e.g: english)
+# These files are encoded in UTF-16LE which is interpreted as binary in Git,
+# and therefore not visible when using 'git diff'
+
+echo "Regenerating Localizable.strings..."
+find src -name '*.mm' | xargs genstrings -o ui/Base.lproj
+iconv -f UTF-16LE -t UTF-8 ui/base.lproj/Localizable.strings > ui/Base.lproj/Localizable.strings.8
+mv ui/Base.lproj/Localizable.strings.8 ui/Base.lproj/Localizable.strings
+
+# generate strings from XIBs
+
+for file in `find ui -name '*.xib' -and -path '*/Base.lproj/*'`; do
+    strings_file=`echo $file | sed s/\.xib/.strings/`
+    echo "Regenerating $strings_file..."
+    ibtool --generate-strings-file $strings_file $file
+    iconv -f UTF-16LE -t UTF-8 $strings_file > $strings_file.8
+    mv $strings_file.8 $strings_file
+done
