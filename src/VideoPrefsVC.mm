f815fafd96548ee9011c31c73eb9e832c61b2ae0
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2015-09-22 17:36:53 -0400

    video: adapt to LRC api change
    Must be merged on top of LRC patchset:
    https://gerrit-ring.savoirfairelinux.com/#/c/2773/
    
    Issue: #80643
    Change-Id: I768d8e85bff1f7b944403d74924e9f7ca495dde8

--- src/VideoPrefsVC.mm
+++ src/VideoPrefsVC.mm
@@ -165,10 +165,10 @@ QMetaObject::Connection previewStopped;
 
 -(void) renderer: (Video::Renderer*)renderer renderFrameForView:(NSView*) view
 {
-    auto data = renderer->currentSmartFrame();
+    auto data = renderer->currentFrame();
     QSize res = renderer->size();
 
-    auto buf = reinterpret_cast<const unsigned char*>(data->data());
+    auto buf = reinterpret_cast<const unsigned char*>(data.data());
 
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
     CGContextRef newContext = CGBitmapContextCreate((void *)buf,
