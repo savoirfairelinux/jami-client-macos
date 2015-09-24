859464cbfb03eb8367d736453aa2a80184b06e51
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2015-09-24 17:09:24 -0400

    buttonnns
    Change-Id: I82fd0189da0ead41e87de91d4a8123a968d79fea

---
+++ src/views/IconButton.h
@@ -0,0 +1,17 @@
+//
+//  IconButton.h
+//  Ring
+//
+//  Created by Alexandre Lision on 24/09/2015.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface IconButton : NSButton
+
+@property (nonatomic) BOOL mouseHovering;
+@property (nonatomic) BOOL mouseDown;
+
+@end
