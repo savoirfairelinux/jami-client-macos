6200531f18e9e76910e0a6f9e12e782fafd5932d
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2016-01-28 15:55:16 -0500

    call: add dialpad access
    This commit adds the ability to dial dtmf tones during a call.
    It is necessary for interacting with automated services.
    RTP dtmf are not working in the daemon, so during SIP account creation
    we switch to SIP dtmf.
    
    Tuleap: #154
    Change-Id: I9d6758af4bc0b44605dc4b17c08b9b4ceaee84bd

--- src/AppDelegate.mm
+++ src/AppDelegate.mm
@@ -35,6 +35,7 @@
 
 #import "Constants.h"
 #import "RingWizardWC.h"
+#import "DialpadWC.h"
 
 #if ENABLE_SPARKLE
 @interface AppDelegate() <SUUpdaterDelegate>
@@ -44,6 +45,7 @@
 
 @property RingWindowController* ringWindowController;
 @property RingWizardWC* wizard;
+@property DialpadWC* dialpad;
 @property (nonatomic, strong) dispatch_queue_t scNetworkQueue;
 @property (nonatomic, assign) SCNetworkReachabilityRef currentReachability;
 
@@ -198,13 +200,6 @@ static void ReachabilityCallback(SCNetworkReachabilityRef __unused target, SCNet
     }
 }
 
-/**
- * click in MainMenu "Setup Ring"
- */
-- (IBAction)showWizard:(id)sender {
-    [self showWizard];
-}
-
 - (void) showWizard
 {
     if(self.wizard == nil) {
@@ -220,6 +215,15 @@ static void ReachabilityCallback(SCNetworkReachabilityRef __unused target, SCNet
     }
     [self.ringWindowController.window makeKeyAndOrderFront:self];
 }
+
+- (void) showDialpad
+{
+    if (self.dialpad == nil) {
+        self.dialpad = [[DialpadWC alloc] initWithWindowNibName:@"Dialpad"];
+    }
+    [self.dialpad.window makeKeyAndOrderFront:self];
+}
+
 
 - (BOOL) checkForRingAccount
 {
