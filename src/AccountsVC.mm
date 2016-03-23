2f9fb29fce6ef89eea0f07b3bbbe847b2590e03a
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2016-03-23 17:24:19 -0400

    accounts: import/export
    Allow import/export of accounts in the preferences.
    PathPasswordWC is a generic modal window asking for a path and a
    password. It is used for both export and import UI.
    
    Tuleap: #335
    Change-Id: Ic478140e64b51d10672ef466509326fc17be2712

--- src/AccountsVC.mm
+++ src/AccountsVC.mm
@@ -23,6 +23,7 @@
 #import <QItemSelectionModel>
 #import <QSortFilterProxyModel>
 #import <QtCore/qdir.h>
+#import <QtCore/qfile.h>
 #import <QtCore/qstandardpaths.h>
 
 // LRC
@@ -36,6 +37,7 @@
 #import "AccAdvancedVC.h"
 #import "AccSecurityVC.h"
 #import "AccRingVC.h"
+#import "PathPasswordWC.h"
 
 // We disabled IAX protocol for now, so don't show it to the user
 class ActiveProtocolModel : public QSortFilterProxyModel
@@ -51,7 +53,8 @@ public:
     }
 };
 
-@interface AccountsVC ()
+@interface AccountsVC () <PathPasswordDelegate>
+
 @property (assign) IBOutlet NSPopUpButton *protocolList;
 
 @property (assign) IBOutlet NSTabView *configPanels;
@@ -65,12 +68,14 @@ public:
 @property ActiveProtocolModel* proxyProtocolModel;
 @property (assign) IBOutlet NSOutlineView *accountsListView;
 @property (assign) IBOutlet NSTabView *accountDetailsView;
+@property (unsafe_unretained) IBOutlet NSButton* exportAccountButton;
 
 @property AccRingVC* ringVC;
 @property AccGeneralVC* generalVC;
 @property AccMediaVC* audioVC;
 @property AccAdvancedVC* advancedVC;
 @property AccSecurityVC* securityVC;
+@property PathPasswordWC* passwordWC;
 
 @end
 
@@ -86,11 +91,17 @@ public:
 @synthesize accountDetailsView;
 @synthesize treeController;
 @synthesize proxyProtocolModel;
+@synthesize passwordWC;
 
 NSInteger const TAG_CHECK       =   100;
 NSInteger const TAG_NAME        =   200;
 NSInteger const TAG_STATUS      =   300;
 NSInteger const TAG_TYPE        =   400;
+
+typedef NS_ENUM(NSUInteger, Action) {
+    ACTION_EXPORT = 0,
+    ACTION_IMPORT = 1,
+};
 
 - (void)awakeFromNib
 {
@@ -233,6 +244,34 @@ NSInteger const TAG_TYPE        =   400;
     [configPanels insertTabViewItem:advancedTabItem atIndex:2];
 
 }
+- (IBAction)exportAccount:(id)sender
+{
+    passwordWC = [[PathPasswordWC alloc] initWithDelegate:self actionCode:Action::ACTION_EXPORT];
+#if MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_9
+    [self.view.window beginSheet:passwordWC.window completionHandler:nil];
+#else
+    [NSApp beginSheet: passwordWC.window
+       modalForWindow: self.view.window
+        modalDelegate: self
+       didEndSelector: nil
+          contextInfo: nil];
+#endif
+    [passwordWC setAllowFileSelection:NO];
+}
+
+- (IBAction)importAccount:(id)sender
+{
+    passwordWC = [[PathPasswordWC alloc] initWithDelegate:self actionCode:Action::ACTION_IMPORT];
+#if MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_9
+    [self.view.window beginSheet:passwordWC.window completionHandler:nil];
+#else
+    [NSApp beginSheet: passwordWC.window
+       modalForWindow: self.view.window
+        modalDelegate: self
+       didEndSelector: nil
+          contextInfo: nil];
+#endif
+}
 
 - (IBAction)toggleAccount:(NSButton*)sender {
     NSInteger row = [accountsListView rowForView:sender];
@@ -312,6 +351,7 @@ NSInteger const TAG_TYPE        =   400;
 - (void)outlineViewSelectionDidChange:(NSNotification *)notification
 {
     // ask the tree controller for the current selection
+    [self.exportAccountButton setEnabled:[[treeController selectedNodes] count] > 0];
     if([[treeController selectedNodes] count] > 0) {
         auto qIdx = [treeController toQIdx:[treeController selectedNodes][0]];
         //Update details view
@@ -335,6 +375,67 @@ NSInteger const TAG_TYPE        =   400;
         [self.accountDetailsView setHidden:NO];
     } else {
         AccountModel::instance().selectionModel()->clearCurrentIndex();
+    }
+}
+
+-(void) didCompleteWithPath:(NSURL*) path Password:(NSString*) password ActionCode:(NSInteger)requestCode
+{
+    switch (requestCode) {
+        case Action::ACTION_EXPORT:
+            if(treeController.selectedNodes.count > 0) {
+                QStringList accounts;
+                for (id item : [treeController selectedNodes]) {
+                    QModelIndex accIdx = [treeController toQIdx:item];
+                    accounts << AccountModel::instance().getAccountByModelIndex(accIdx)->id();
+
+                }
+                auto finalURL = [path URLByAppendingPathComponent:@"accounts.ring"];
+                [passwordWC showLoading];
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                    int result = AccountModel::instance().exportAccounts(accounts, finalURL.path.UTF8String, password.UTF8String);
+                    switch (result) {
+                        case 0:
+                            [[NSWorkspace sharedWorkspace] selectFile:finalURL.path inFileViewerRootedAtPath:@""];
+                            [passwordWC close];
+                            break;
+                        default:
+                            [passwordWC showError:NSLocalizedString(@"An error occured during the export", @"Error shown to the user" )];
+                            break;
+                    }
+                });
+            }
+            break;
+        case Action::ACTION_IMPORT: {
+            [passwordWC showLoading];
+            SEL sel = @selector(importAccountsWithPath:andPassword:);
+            NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[self methodSignatureForSelector:sel]];
+            [inv setSelector:sel];
+            [inv setTarget:self];
+            //arguments 0 and 1 are self and _cmd respectively, automatically set by NSInvocation
+            [inv setArgument:&path atIndex:2];
+            [inv setArgument:&password atIndex:3];
+
+            // Schedule import for next iteration of event loop in order to let us start the loading anim
+            [inv performSelector:@selector(invoke) withObject:nil afterDelay:0];
+
+        }
+            break;
+        default:
+            NSLog(@"Unrecognized action %d", requestCode);
+            break;
+    }
+}
+
+- (void) importAccountsWithPath:(NSURL*) path andPassword:(NSString*) password
+{
+    int result = AccountModel::instance().importAccounts(path.path.UTF8String, password.UTF8String);
+    switch (result) {
+        case 0:
+            [passwordWC close];
+            break;
+        default:
+            [passwordWC showError:NSLocalizedString(@"An error occured during the import", @"Error shown to the user" )];
+            break;
     }
 }
 
