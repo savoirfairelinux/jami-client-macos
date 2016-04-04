261f1b917b6ec35e83f6d14edb91d0d2a344e0d8
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2016-04-04 12:35:34 -0400

    profile: add edition in settings
    - fix a small offset of the preference window when
    switching between tabs
    - increase sizes of photos to avoid pixelisation
    
    Change-Id: Idfb9149a8de08c034dff3e42b41f5888bba893f7
    Tuleap: #530

---
+++ src/views/NSImage+Extensions.mm
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *  Author: Alexandre Lision <alexandre.lision@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#import "NSImage+Extensions.h"
+
+@implementation NSImage (Extensions)
+
++ (NSImage *)imageResize:(NSImage*)anImage
+                 newSize:(NSSize)newSize
+{
+    auto sourceImage = anImage;
+    // Report an error if the source isn't a valid image
+    if (![sourceImage isValid]) {
+        NSLog(@"Invalid Image");
+    } else {
+        auto smallImage = [[NSImage alloc] initWithSize: newSize];
+        [smallImage lockFocus];
+        [sourceImage setSize: newSize];
+        [[NSGraphicsContext currentContext] setImageInterpolation:NSImageInterpolationHigh];
+        [sourceImage drawAtPoint:NSZeroPoint fromRect:NSZeroRect operation:NSCompositeCopy fraction:1.];
+        [smallImage unlockFocus];
+        return smallImage;
+    }
+    return nil;
+}
+
+@end
