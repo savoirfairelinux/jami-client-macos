71b4151776adf28fd05bb470a54a1305e2b451c0
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2016-05-02 13:53:44 -0400

    packaging: update scripts to run on OSX
    Some command line options differ between the GNU and UNIX version of
    base64, date and stat
    
    Change-Id: I5d7ec0a3e797a1044e9acae3153caf9cb9eef00e
    Tuleap: #634

--- sparkle/sign_update.sh
+++ sparkle/sign_update.sh
@@ -5,6 +5,11 @@ if [ "$#" -ne 2 ]; then
   echo "Usage: $0 update_archive private_key"
   exit 1
 fi
+
+if [[ `uname` == 'Linux' ]]; then
+    BASE64_OPTS='--wrap=0'
+fi
+
 openssl=/usr/bin/openssl
-$openssl dgst -sha1 -binary < "$1" | $openssl dgst -dss1 -sign "$2" | base64 --wrap=0
+$openssl dgst -sha1 -binary < "$1" | $openssl dgst -dss1 -sign "$2" | base64 $BASE64_OPTS
 
