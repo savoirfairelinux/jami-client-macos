48e9573b69a1b54edd88faa4434f8423b1a051d1
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2016-08-25 15:16:17 -0400

    wip prepare export on ring multidevice
    add tab panel for Ring accounts
    
    Change-Id: I3116d68f854160738a73e1faca3099e1e5eb47d5
    Tuleap: #959

---
+++ src/AccDevicesVC.m
@@ -0,0 +1,22 @@
+//
+//  AccDevicesVC.m
+//  Ring
+//
+//  Created by Alexandre Lision on 2016-08-25.
+//
+//
+
+#import "AccDevicesVC.h"
+
+@interface AccDevicesVC ()
+
+@end
+
+@implementation AccDevicesVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
