d7147950909421c69d88b0d4b0e42d36d4a6026f
Loïc Siret <loic.siret@savoirfairelinux.com>
2016-09-08 14:38:24 -0400

    pathpasswordvc: refactoring/extract superclass
    This patch extracts superclass and protocols from PathPasswordVC.
    Allowing to reuse code for futur components needed in futur release
    like AddingDevices.
    
    Change-Id: I9cc8c88489f14841e5c8f36cc2fa2eb9d967b1b0
    Tuleap: #959

---
+++ LoadingVCDelegate.h
@@ -0,0 +1,13 @@
+//
+//  LoadingViewDelegate.h
+//  Ring
+//
+//  Created by Loïc Siret on 2016-09-08.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol LoadingViewDelegate <NSObject>
+
+@end
