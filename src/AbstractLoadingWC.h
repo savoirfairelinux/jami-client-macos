af4b72d4bef74339fa1a6d87c06e8d57d588c148
Loïc Siret <loic.siret@savoirfairelinux.com>
2016-09-08 14:38:24 -0400

    pathpasswordvc: refactoring/extract superclass
    This patch extracts superclass and protocols from PathPasswordVC.
    Allowing to reuse code for futur components needed by futur releases
    like AddingDevices.
    
    Change-Id: I9cc8c88489f14841e5c8f36cc2fa2eb9d967b1b0
    Tuleap: #959

---
+++ src/AbstractLoadingWC.h
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (C) 2015-2016 Savoir-faire Linux Inc.
+ *  Author: Loïc Siret <loic.siret@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#import <Cocoa/Cocoa.h>
+#import "LoadingWCProtocol.h"
+#import "views/ITProgressIndicator.h"
+
+@interface AbstractLoadingWC :  NSWindowController <LoadingWCProtocol>
+{
+@protected
+    __unsafe_unretained IBOutlet NSView* errorContainer;
+    __unsafe_unretained IBOutlet NSTextField* errorLabel;
+
+    __unsafe_unretained IBOutlet ITProgressIndicator* progressView;
+
+    __unsafe_unretained IBOutlet NSView* initialContainer;
+    __unsafe_unretained IBOutlet NSView* finalContainer;
+}
+
+/*
+ * Delegate to inform about completion of the linking process between
+ * a ContactMethod and a Person.
+ */
+@property (retain, nonatomic) id <LoadingWCDelegate> delegate;
+
+/*
+ * caller specific code to identify ongoing action
+ */
+@property (nonatomic) NSInteger actionCode;
+
+
+- (id)initWithWindowNibName:(NSString *)nibName
+                   delegate:(id <LoadingWCDelegate>) del
+                 actionCode:(NSInteger) code;
+
+
+@end
