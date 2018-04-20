72a26c3427cbcf443e69d08f9c2ca3c5739f74e7
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2018-04-20 16:36:20 -0400

    UI/UX: refactor smartlist
    Change-Id: Ibfd5154757908ebd85f4b0060da00a7c608a0e56

--- src/MessagesVC.mm
+++ src/MessagesVC.mm
@@ -393,8 +393,6 @@ typedef NS_ENUM(NSInteger, MessageSequencing) {
             } else {
                 result = [tableView makeViewWithIdentifier:@"LeftMessageView" owner:self];
             }
-            if (interaction.status == lrc::api::interaction::Status::UNREAD)
-                convModel_->setInteractionRead(convUid_, it->first);
             break;
         case lrc::api::interaction::Type::INCOMING_DATA_TRANSFER:
         case lrc::api::interaction::Type::OUTGOING_DATA_TRANSFER:
