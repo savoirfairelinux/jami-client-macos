172733aeebfee9bd4ae7b2a9df1ba633d0ee1a83
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2018-11-12 17:49:01 -0500

    new lrc: link to avModel
    Change-Id: I0a904813749d3ddc645878fad107aacdee9cd9bc

--- src/AppDelegate.mm
+++ src/AppDelegate.mm
@@ -242,7 +242,7 @@ static void ReachabilityCallback(SCNetworkReachabilityRef __unused target, SCNet
 - (void) showMainWindow
 {
     if(self.ringWindowController == nil) {
-        self.ringWindowController = [[RingWindowController alloc] initWithWindowNibName:@"RingWindow" bundle: nil accountModel:&lrc->getAccountModel() dataTransferModel:&lrc->getDataTransferModel() behaviourController:&lrc->getBehaviorController()];
+        self.ringWindowController = [[RingWindowController alloc] initWithWindowNibName:@"RingWindow" bundle: nil accountModel:&lrc->getAccountModel() dataTransferModel:&lrc->getDataTransferModel() behaviourController:&lrc->getBehaviorController() avModel: &lrc->getAVModel()];
     }
     [[NSApplication sharedApplication] removeWindowsItem:self.wizard.window];
     [self.ringWindowController.window makeKeyAndOrderFront:self];
