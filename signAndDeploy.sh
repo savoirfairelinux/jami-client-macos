85a334e9e117acd722bf9eb2a325360b0663f5f0
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2018-12-03 15:54:19 -0500

    project: rename Ring to Jami
    Change-Id: Id137adf6aa12f6d7b764c25620f0ef73882d8bfa
    Reviewed-by: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>

--- signAndDeploy.sh
+++ signAndDeploy.sh
@@ -14,11 +14,11 @@ security import certificates/certificates/distribution/Certificates.p12 -k $KEYC
 DELIVER_PASSWORD=$APPLE_PASSWORD fastlane sigh --app_identifier $BUNDLE_ID --username $APPLE_ACCOUNT --readonly true --platform macos --team_id $TEAM_ID
 security set-key-partition-list -S apple-tool:,apple:,productbuild: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN_NAME > /dev/null 2>&1
 echo "start signing"
-macdeployqt ./Ring.app -codesign="${APP_CERTIFICATE}"
-codesign --force --sign "${APP_CERTIFICATE}" --entitlements ../data/Ring.entitlements Ring.app
-codesign --verify Ring.app
+macdeployqt ./Jami.app -codesign="${APP_CERTIFICATE}"
+codesign --force --sign "${APP_CERTIFICATE}" --entitlements ../data/Jami.entitlements Jami.app
+codesign --verify Jami.app
 echo "create .pkg"
-productbuild --component Ring.app/ /Applications --sign "${INSTALLER_CERTIFICATE}" --product Ring.app/Contents/Info.plist Ring.pkg
-/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool  --validate-app  --type osx -f Ring.pkg -u $APPLE_ACCOUNT --password $APPLE_PASSWORD
+productbuild --component Jami.app/ /Applications --sign "${INSTALLER_CERTIFICATE}" --product Jami.app/Contents/Info.plist Jami.pkg
+/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool  --validate-app  --type osx -f Jami.pkg -u $APPLE_ACCOUNT --password $APPLE_PASSWORD
 echo "start deploying"
-/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool  --upload-app  --type osx -f Ring.pkg -u $APPLE_ACCOUNT --password $APPLE_PASSWORD
+/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool  --upload-app  --type osx -f Jami.pkg -u $APPLE_ACCOUNT --password $APPLE_PASSWORD
