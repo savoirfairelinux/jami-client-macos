38291f93e1bc20692caa9f9ba0d12c191e5593e5
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2019-10-02 10:37:54 -0400

    videoView: force redraw
    If window is closed when video running metal view stop
    redrawing. This patch force view redraw after setting a new frame.
    
    Change-Id: If6f1266510061dd7b9d04e95cacf58395fdb4220

--- src/views/CallMTKView.mm
+++ src/views/CallMTKView.mm
@@ -215,6 +215,7 @@ bool frameDisplayed = false;
     [commandEncoder endEncoding];
     [commandBuffer presentDrawable:drawable];
     [commandBuffer commit];
+    [self draw];
 }
 
 -(simd::float4x4) getScalingMatrix:(CGFloat) ratio axis:(char) axis {
