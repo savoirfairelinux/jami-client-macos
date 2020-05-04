4267ef6dd80f651c76542e6337d768dcebc7e91b
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2020-05-04 09:13:27 -0400

    video: use openGL
    Change-Id: Ic2d19d19da72b71f73f12d3efab75c9bb384d2fb

---
+++ src/views/RenderProtocol.h
@@ -0,0 +1,31 @@
+/*
+*  Copyright (C) 2020 Savoir-faire Linux Inc.
+*  Author: Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program; if not, write to the Free Software
+*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+*/
+
+#import <Foundation/Foundation.h>
+#import <video/renderer.h>
+@protocol RenderProtocol
+-(void)setCurrentFrame:(lrc::api::video::Frame)framePtr;
+-(void)renderWithPixelBuffer:(CVPixelBufferRef)buffer size:(CGSize)size rotation: (float)rotation fillFrame: (bool)fill;
+-(void)fillWithBlack;
+-(void)setupView;
+@property BOOL videoRunning;
+-(void)renderAVFrame:(std::unique_ptr<AVFrame, void(*)(AVFrame*)>)buffer;
+
+@end
+
