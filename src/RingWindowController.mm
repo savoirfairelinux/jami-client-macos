80cc4c4af25394246f622be29fe7ff375a03c417
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2020-08-11 14:29:26 -0400

    conversations: user search
    Change-Id: Icb5c130e8fa2fe7d2691ae7e9b1abaacb78e8bbc

--- src/RingWindowController.mm
+++ src/RingWindowController.mm
@@ -609,9 +609,8 @@ typedef NS_ENUM(NSInteger, ViewState) {
     [smartViewVC setConversationModel:accInfo.conversationModel.get()];
     [smartViewVC selectConversationList];
     [self updateRingID];
-    auto convInfo = getConversationFromUid(QString::fromNSString(conversationId), *accInfo.conversationModel.get());
-    auto convQueue = accInfo.conversationModel.get()->allFilteredConversations();
-    if (convInfo != convQueue.end()) {
+    auto convInfo = getConversationFromUid(QString::fromNSString(conversationId), *accInfo.conversationModel.get(), true);
+    if (conversationExists(convInfo, *accInfo.conversationModel.get(), true)) {
         [conversationVC setConversationUid:convInfo->uid model:accInfo.conversationModel.get()];
         [smartViewVC selectConversation: *convInfo model:accInfo.conversationModel.get()];
         accInfo.conversationModel.get()->clearUnreadInteractions(QString::fromNSString(conversationId));
@@ -625,9 +624,8 @@ typedef NS_ENUM(NSInteger, ViewState) {
     [settingsVC setSelectedAccount:accInfo.id];
     [smartViewVC setConversationModel:accInfo.conversationModel.get()];
     [self updateRingID];
-    auto convInfo = getConversationFromUid(QString::fromNSString(conversationId), *accInfo.conversationModel.get());
-    auto convQueue = accInfo.conversationModel.get()->allFilteredConversations();
-    if (convInfo != convQueue.end()) {
+    auto convInfo = getConversationFromUid(QString::fromNSString(conversationId), *accInfo.conversationModel.get(), true);
+    if (conversationExists(convInfo, *accInfo.conversationModel.get(), true)) {
         if (accInfo.contactModel->getContact(convInfo->participants[0]).profileInfo.type == lrc::api::profile::Type::PENDING)
             [smartViewVC selectPendingList];
         else
