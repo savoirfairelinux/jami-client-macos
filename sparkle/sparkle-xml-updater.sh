723d95da640099f25954483095bf59cf1b6923b4
kkostiuk <kateryna.kostiuk@savoirfairelinux.com>
2021-04-08 21:35:26 -0400

    sparkle: update minimum required version
    Change-Id: Ib1e790f63f6753eeee908f957d5319f9c7302acc

--- sparkle/sparkle-xml-updater.sh
+++ sparkle/sparkle-xml-updater.sh
@@ -37,7 +37,7 @@ cat << EOFILE > ${REPO_FOLDER}/${SPARKLE_FILE}
             <title>Ring nightly $(date "+%Y/%m/%d %H:%M")</title>
             <pubDate>$DATE_RFC2822</pubDate>
             <enclosure url="${REPO_URL}/$(basename ${PACKAGE})" sparkle:version="$(date +%Y%m%d)" sparkle:shortVersionString="nightly-$(date "+%Y%m%d")" length="$PACKAGE_SIZE" type="application/octet-stream" sparkle:dsaSignature="$(./sign_update.sh ${PACKAGE} ${DSA_KEY})" />
-            <sparkle:minimumSystemVersion>10.7</sparkle:minimumSystemVersion>
+            <sparkle:minimumSystemVersion>10.13</sparkle:minimumSystemVersion>
         </item>
 $(echo -e "${ITEMS}")
     </channel>
