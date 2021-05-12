6fb72d5a7fb041271c286eaaf8530803f625f620
agsantos <aline.gondimsantos@savoirfairelinux.com>
2021-05-12 12:55:51 -0400

    plugins: add preferences to settings view
    Change-Id: I8296c7b145b76fe6025255bedf326c091530b1c1

---
+++ src/views/NoResponderTableView.h
@@ -0,0 +1,16 @@
+//
+//  NoResponderTableView.h
+//  Jami
+//
+//  Created by jami on 2021-05-12.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NoResponderTableView : NSScrollView
+
+@end
+
+NS_ASSUME_NONNULL_END
